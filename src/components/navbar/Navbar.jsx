190255144e5b268677af5eb1944a0dc0392bf54c
Cypher1998 <codedshegzy@gmail.com>
2022-03-02 23:05:19 +0100

    small screen navbar

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,26 @@
+import { BsPerson, BsCart } from 'react-icons/bs';
+import { AiOutlineHome, AiOutlineAlignLeft } from 'react-icons/ai';
+
+const Navbar = () => {
+  return (
+    <div className="mainNavbar py-4 d-lg-none px-md-5">
+      <div className="myContainer d-flex justify-content-between">
+        <div className="">
+          <AiOutlineAlignLeft size={25} className="iconStyle" />
+        </div>
+        <div>
+          <AiOutlineHome size={25} className="iconStyle" />
+        </div>
+        <div className="position-relative">
+          <BsCart size={25} className="iconStyle" />
+          <span class="itemNumber">1</span>
+        </div>
+        <div className="">
+          <BsPerson size={25} className="iconStyle" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,11 @@
 import SearchNavbar from '../components/navbarsearch/molecule/SearchNavbar';
+import Navbar from '../components/navbar/Navbar';
 
 const Home = () => {
   return (
     <>
       <SearchNavbar />
-      {/* <Navbar /> */}
+      <Navbar />
     </>
   );
 };
