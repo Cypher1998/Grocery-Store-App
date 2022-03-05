a881920f6d310fab62953b4a8b015910b742c3b8
Cypher1998 <codedshegzy@gmail.com>
2022-03-05 13:30:15 +0100

    some changes

--- src/components/featurebackground/FeatBackground.jsx
+++ src/components/featurebackground/FeatBackground.jsx
@@ -44,9 +44,30 @@ const FeatBackground = ({
                       </div>
                       <div className="text">
                         <p>{title}</p>
-                        <ul>
+                        <ul className="p-0 m-0">
                           {subtitle.map((listItem) => (
-                            <li>{listItem}</li>
+                            <li>
+                              <span>
+                                <svg
+                                  stroke="currentColor"
+                                  fill="currentColor"
+                                  stroke-width="0"
+                                  viewBox="0 0 512 512"
+                                  height="1em"
+                                  width="1em"
+                                  xmlns="http://www.w3.org/2000/svg"
+                                >
+                                  <path
+                                    fill="none"
+                                    stroke-linecap="square"
+                                    stroke-miterlimit="10"
+                                    stroke-width="48"
+                                    d="M184 112l144 144-144 144"
+                                  ></path>
+                                </svg>
+                                {listItem}
+                              </span>
+                            </li>
                           ))}
                         </ul>
                       </div>
